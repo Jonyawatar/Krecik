c5e97c010b6ca37c564d7510785ed58cb8876699
Jonyawatar <60350079+Jonyawatar@users.noreply.github.com>
2020-06-11 18:12:22 +0200

    test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+	$liczba = 21;
+	$string = "liczba jest";
+	echo $string;
+	echo '<br>';
+	
+
+	if ($liczba % 2) {
+		$string = $string. "nieparzysta";
+	} else {
+		$string = $string. "parzysta";
+	}
+	echo $string
+
+?>
